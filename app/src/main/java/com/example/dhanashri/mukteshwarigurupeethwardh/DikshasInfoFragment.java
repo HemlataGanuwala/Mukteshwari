5d489ee046a9bc2f18aba49fe5564ef8b9088df3
Dhanashri <dhanashrihemane78@gmail.com>
2018-12-11 19:20:20 +0530

    d1

--- app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/DikshasInfoFragment.java
+++ app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/DikshasInfoFragment.java
@@ -6,12 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DikshasInfoFragment extends Fragment {
+
+    View view;
+    EditText dod, annual_member, no_of_annual_attempt, last_insentive_date, location, area_of_zone, zonal_head, zonal_head_contact_no, destination;
 
 
     public DikshasInfoFragment() {
@@ -23,7 +27,20 @@ public class DikshasInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dikshas_info, container, false);
+
+        view = inflater.inflate(R.layout.fragment_dikshas_info, container, false);
+
+        dod=(EditText)view.findViewById(R.id.etdob);
+        annual_member=(EditText)view.findViewById(R.id.etannualmember);
+        no_of_annual_attempt=(EditText)view.findViewById(R.id.etattempt);
+        last_insentive_date=(EditText)view.findViewById((R.id.etlastinsentivedate));
+        location=(EditText)view.findViewById(R.id.etlocation);
+        area_of_zone=(EditText)view.findViewById(R.id.etareazone);
+        zonal_head=(EditText)view.findViewById(R.id.etzonalhead);
+        zonal_head_contact_no=(EditText)view.findViewById(R.id.etzonalheadno);
+        destination=(EditText)view.findViewById(R.id.etdestination);
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/PersonalInfoActivity.java
+++ app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/PersonalInfoActivity.java
@@ -1,14 +1,21 @@
 package com.example.dhanashri.mukteshwarigurupeethwardh;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +24,56 @@ public class PersonalInfoActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout)findViewById(R.id.tablayout);
         viewPager = (ViewPager)findViewById(R.id.viewpager);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.AddFragment(new PersonalInfoFragment(),"Personal Information");
         adapter.AddFragment(new DikshasInfoFragment(),"Dikshya Information");
         adapter.AddFragment(new TotalPersonInfoFragment(),"Total Person Information");
+        adapter.AddFragment(new  PhotoFragment(),"Photos");
 
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+        createTab();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+       return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_back:
+                Intent intent = new Intent(PersonalInfoActivity.this, DashboardActivity.class);
+                startActivity(intent);
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void createTab()
+    {
+
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("Personal Information");
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setText("Dikshya Information");
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setText("Total Person Information");
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+
+        TextView tabFour = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabFour.setText("Photos");
+        tabLayout.getTabAt(3).setCustomView(tabFour);
+
     }
 }
