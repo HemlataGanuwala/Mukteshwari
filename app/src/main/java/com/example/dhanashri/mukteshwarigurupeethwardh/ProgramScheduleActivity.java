a9e555e20f23b8c42e2a0bf0813b4cdacb71d818
Shweta <1192abhilas@gmail.com>
2018-12-13 20:45:17 +0530

    s5

--- app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/ProgramScheduleActivity.java
+++ app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/ProgramScheduleActivity.java
@@ -1,13 +1,37 @@
 package com.example.dhanashri.mukteshwarigurupeethwardh;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProgramScheduleActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_schedule);
+
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new SpecialProgramFragment(),"SpecialProgram");
+        adapter.AddFragment(new MantradikshaFragment(),"Mantradiksha");
+        adapter.AddFragment(new SukshmdyanFragment(),"Sukshmdyan");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/SpecialProgramFragment.java
+++ app/src/main/java/com/example/dhanashri/mukteshwarigurupeethwardh/SpecialProgramFragment.java
@@ -6,14 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SpecialProgramFragment extends Fragment {
-
-
+    View view;
+    WebView webView;
+    WebSettings webSettings;
     public SpecialProgramFragment() {
         // Required empty public constructor
     }
@@ -22,8 +25,15 @@ public class SpecialProgramFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_special_program, container, false);
+        webView = (WebView)view.findViewById(R.id.webview);
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://spcialprogram.skyvisioncables.com/");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_special_program, container, false);
+        return view ;
     }
 
 }
